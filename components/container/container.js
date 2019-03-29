3ee7b21c7cb385eceed2287e478996f7fc3f2864
fnix-lu <835913098@qq.com>
2019-03-30 01:10:12 +0800

    小程序基础样式

---
+++ components/container/container.js
@@ -0,0 +1,29 @@
+// components/container/container.js
+Component({
+  /**
+   * 组件的选项列表
+   */
+  options: {
+    addGlobalClass: true
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/circle/circle.js
+++ pages/circle/circle.js
@@ -1,4 +1,4 @@
-// pages/index/index.js
+// pages/circle/circle.js
 Page({
 
   /**
