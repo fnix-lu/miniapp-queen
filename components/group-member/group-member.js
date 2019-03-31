096195f66905a1a904dc756836639d4f05fb960a
fnix-lu <835913098@qq.com>
2019-03-31 21:33:24 +0800

    新增我的订单页

---
+++ components/group-member/group-member.js
@@ -0,0 +1,33 @@
+// components/group-member/group-member.js
+Component({
+  options: {
+    addGlobalClass: true
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    isLeader: {
+      type: Boolean,
+      value: false
+    },
+    isFirst: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ pages/order/order.js
@@ -0,0 +1,77 @@
+// pages/order/order.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  handleTap () {
+    wx.showModal({
+      title: '提示',
+      content: '这是按钮的点击事件',
+      cancelText: '再等等',
+      cancelColor: '#FC7B7B',
+      confirmText: '确认取消',
+      confirmColor: '#888888'
+    })
+  }
+})
