ff4a701016bfdd905ee63412a4e2511a6c8bbb32
fnix-lu <835913098@qq.com>
2019-04-09 18:11:26 +0800

    接口封装

--- api/api.js
+++ api/api.js
@@ -1,23 +1,30 @@
 const HOST = 'http://39.98.241.13:997/api'
 
-const fetch = (url, data) => new Promise((resolve, reject) => {
+const fetch = (url, data = {}, method = 'GET') => new Promise((resolve, reject) => {
   wx.request({
     url: `${HOST}/${url}`,
     data: {
-      Plantform: 1,
+      Platform: 1,
       ...data
     },
-    success: res => {
-      if (res.code === 200) {
-        resolve(res.data)
-      }
+    header: {
+      'content-type': 'application/json'
     },
-    fail: reject(res)
+    method,
+    success (res) {
+      res.statusCode === 200 ? resolve(res.data) : reject(res)
+    }
   })
 })
 
 module.exports = {
-  getBrands () {
-    return fetch('product/GetProducts')
+  // 获取品牌列表
+  getBrands (data) {
+    return fetch('product/GetProductBrands', data)
+  },
+  
+  // 获取商品列表
+  getGoods (data) {
+    return fetch('product/GetProducts', data)
   }
 }
--- pages/category/category.js
+++ pages/category/category.js
@@ -1,33 +1,51 @@
 // pages/category/category.js
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    flagShowCart: false,
-    flagShowCover: false
+    flag: {
+      showCart: false,
+      showCover: false
+    },
+    currentBrandId: '',
+    brand: {
+      currentPage: 0,
+      allPageCount: 1,
+      list: []
+    },
+    goods: {
+      currentPage: 0,
+      allPageCount: 1
+    },
+    goodsMap: {
+      'brand-id': []
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const _this = this
+    _this.getBrands()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
@@ -70,8 +88,69 @@ Page({
    */
   toggleShowCart () {
     this.setData({
-      flagShowCover: !this.data.flagShowCover,
-      flagShowCart: !this.data.flagShowCart
+      'flag.showCover': !this.data.flag.showCover,
+      'flag.showCart': !this.data.flag.showCart
     })
+  },
+
+  /**
+   * 获取品牌
+   */
+  getBrands () {
+    const _this = this
+    
+    if (_this.data.brand.currentPage >= _this.data.brand.allPageCount) {
+      return
+    }
+    app.api.getBrands({
+      PageIndex: _this.data.brand.currentPage + 1,
+      PageSize: 10
+    }).then(res => {
+      if (res.Code === 1000) {
+        _this.setData({
+          'brand.currentPage': res.PageIndex,
+          'brand.allPageCount': res.AllPageCount,
+          'brand.list': _this.data.brand.list.concat(res.Data),
+        })
+        // 设置初始选中的品牌
+        if (res.PageIndex === 1) {
+          _this.setData({
+            'currentBrandId': res.Data[0].Id
+          })
+        }
+        // 获取初始品牌的商品
+        _this.getGoods()
+      }
+    })
+  },
+
+  /**
+   * 获取商品
+   */
+  getGoods () {
+    const _this = this
+
+    app.api.getGoods({
+      BrandId: _this.data.currentBrandId
+    }).then(res => {
+      console.log(res)
+
+      if (res.Code === 1000) {
+        _this.setData({
+          // 'goodsMap[_this.data.currentBrandId]': _this.data.goodsMap[_this.data.currentBrandId]
+        })
+      }
+    })
+  },
+
+  /**
+   * 切换品牌
+   */
+  changeBrand (e) {
+    this.setData({
+      currentBrandId: e.currentTarget.dataset.id
+    })
+    // 获取对应品牌的商品
+    this.getGoods()
   }
 })
